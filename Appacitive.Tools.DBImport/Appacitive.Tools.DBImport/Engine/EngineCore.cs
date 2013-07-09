54e1000017fce3f064fd74fb13e47a77b1f88296
sathley <sathley@appacitive.com>
2013-07-09 18:55:49 +0530

    some restructuring

---
+++ Appacitive.Tools.DBImport/Appacitive.Tools.DBImport/Engine/EngineCore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Appacitive.Tools.DBImport
+{
+    public static class EngineCore
+    {
+        public static AppacitiveInput AppacitizeDatabase(this Database database)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
