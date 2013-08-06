fab98411432fbb3dc9d328dafd7ef7a8c383ca9b
sathley <sathley@appaciive.com>
2013-08-06 16:22:14 +0530

    fixed tests and added deleted files

--- Appacitive.Tools.DBImport/Appacitive.Tools.DBImport.Tests/WithoutConfigTestFixtures.cs
+++ Appacitive.Tools.DBImport/Appacitive.Tools.DBImport.Tests/WithoutConfigTestFixtures.cs
@@ -1,32 +1,36 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Configuration;
-//using System.Linq;
-//using System.Text;
-//using Appacitive.Tools.DBImport.Model;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using Appacitive.Tools.DBImport.Model;
 
-//namespace Appacitive.Tools.DBImport.Tests
-//{
-//    public class WithoutConfigTestFixtures
-//    {
-//        public void TestWithoutConfigWhisperingUsingMysqlDB()
-//        {
-//            var connectionString = ConfigurationManager.AppSettings["mysql-connectionstring"];
-//            var database = ConfigurationManager.AppSettings["mysql-database"];
-//            var dbImport = new DBImport();
-//            dbImport.Import(new InputConfigurationWithDatabaseDetails()
-//                                {
-//                                    AppacitiveDetails=new AppacitiveDetails()
-//                                              {
-//                                                  DBConnectionString = connectionString,
-//                                                  DBProvider = "mysql",
-//                                                  DatabaseName = database,
-//                                                  BlueprintId = "__EditableBlueprint_AppacitiveApplication2",
-//                                                  APIKey = "2",
-//                                                  AppacitiveBaseURL = ConfigurationManager.AppSettings["appacitive-base-url"]
-//                                              },
-//                                              TableMappings = null
-//                                });
-//        }
-//    }
-//}
+namespace Appacitive.Tools.DBImport.Tests
+{
+    public class WithoutConfigTestFixtures
+    {
+        public void TestWithoutConfigWhisperingUsingMysqlDB()
+        {
+            var connectionString = ConfigurationManager.AppSettings["mysql-connectionstring"];
+            var database = ConfigurationManager.AppSettings["mysql-database"];
+            var dbImport = new DBImport();
+            dbImport.Import(new InputConfigurationWithDatabaseDetails()
+                                {
+                                    AppacitiveDetails = new AppacitiveDetails()
+                                              {
+
+                                                  BlueprintId = "__EditableBlueprint_AppacitiveApplication2",
+                                                  APIKey = "2",
+                                                  AppacitiveBaseURL = ConfigurationManager.AppSettings["appacitive-base-url"]
+                                              },
+                                    DatabaseDetails = new DatabaseDetails()
+                                      {
+                                          DBConnectionString = connectionString,
+                                          DBProvider = "mysql",
+                                          DatabaseName = database,
+                                      },
+                                    TableMappings = null
+                                });
+        }
+    }
+}
